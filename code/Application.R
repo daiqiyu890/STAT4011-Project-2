f4fc5a9778928a66174592c7af254a1bedf1928a
Jiang Yunhui, Doris <dorisjiang4@gmail.com>
2022-12-09 00:30:36 +0800

    update

--- code/Application.R
+++ code/Application.R
@@ -1,24 +1,14 @@
 setwd("/Users/jiangyunhui/Documents/GitHub/STAT4011-Project-2")
 #Step 1: data pre-processing----------------------------------------------------
-#visitors
-data1<-read.csv("data/visitors_rawdata.csv")
 
-samplesize=60
-a<-nrow(data1)
-b<-a-(samplesize-1)
-x1<-data1[b:a,]
-c1<-as.numeric(gsub(",", "", x1$Unique.Visits))
-data1<-matrix(c1,ncol=1)
-colnames(data1)="x"
-
-
+"""
 #accident
 data3<-read.csv("data/accident_rawdata.csv")
 x3<-data3[1:samplesize,]
 c1<-as.numeric(gsub(",", "", x3$Total_Accident))
 data3<-matrix(c1,ncol=1)
 colnames(data3)="x"
-
+"""
 
 #Step 2: Model Selection--------------------------------------------------------
 setwd("/Users/jiangyunhui/Documents/GitHub/STAT4011-Project-2")
@@ -29,7 +19,7 @@ source("code/viterbi_gaus.R")
 source("code/viterbi_pois.R")
 
 #sales
-
+"""
 x2=c(data2$ProductP4)
 gaus_mean_x2=mean(x2)
 gaus_sd_last_x2=sqrt(var(x2))
@@ -56,6 +46,7 @@ for (m in 2:4){
 aic_bic_x2[1,len_table]=as.numeric(which.min(aic_bic_x2[1,])+1)
 aic_bic_x2[2,len_table]=as.numeric(which.min(aic_bic_x2[2,])+1)
 print(aic_bic_x2)
+"""
 
 #energy=======================
 data4=read.csv("/Users/jiangyunhui/Documents/GitHub/STAT4011-Project-2/data/energy_first_60.csv")
@@ -139,7 +130,6 @@ legend("topright", inset=c(-0.25, 0),legend=c("original data", "estimated"),
 # visitor
 #model select
 data1<-read.csv("/Users/jiangyunhui/Documents/GitHub/STAT4011-Project-2/data/visitors_rawdata.csv")
-
 samplesize=60
 a<-nrow(data1)
 b<-a-(samplesize-1)
@@ -153,7 +143,7 @@ for (m in 2:4){
   #m=3
   initP_last=rep(1/m,m)
   transP_last=matrix(0.3/m,m,m)+matrix(c(rep(0.7,m),rep(0,m*(m-1))),m,m)
-  lambda_x1=rep(mean_x1,m)
+  lambda_last=rep(mean_x1,m)
   
   list_mstep_para_pois=EM_pois(T=length(x1),m,x1,lambda_last,
                                transP_last,initP_last,
@@ -169,7 +159,7 @@ aic_bic_x1[1,len_table]=as.numeric(which.min(aic_bic_x1[1,])+1)
 aic_bic_x1[2,len_table]=as.numeric(which.min(aic_bic_x1[2,])+1)
 print(aic_bic_x1)
 #viterbi
-m=2
+m=as.numeric(aic_bic_x1[1,len_table])
 initP_last=rep(1/m,m)
 transP_last=matrix(0.3/m,m,m)+matrix(c(rep(0.7,m),rep(0,m*(m-1))),m,m)
 lambda_last=rep(3000,m)
@@ -197,11 +187,33 @@ plot(path,type = "l",col="red",ylab = "",yaxt='n',xlab = "")
 legend("topright", inset=c(-0.4, 0),legend=c("data", "viterbi result"),
        col=c("black", "red"),lty=1:2,cex = 0.6)
 
+# density compare
+T=500
+s=rep(NA,T)
+xr=rep(NA,T)
+p_t=initP_est_x1
+s[1]=which.max(p_t)
+xr[1]=rpois(1,lambda_est_x1[s[1]])
+for (t in 2:T){
+  p_t=initP_est_x1%*%transP_est_x1
+  s[t]=which.max(p_t)
+  xr[t]=rpois(1,lambda_est_x1[s[t]])
+}
+par(mar = c(2, 2, 2, 7), xpd = TRUE)
+plot(density(x1))
+par(new=TRUE)
+plot(density(xr),col="red",yaxt="n",xaxt="n")
+legend("topright", inset=c(-0.25, 0),legend=c("original data", "estimated"),
+       col=c("black", "red"),lty=1:2,cex = 0.6)
 #accident===============================
 #model select
-data3<-read.csv("/Users/jiangyunhui/Documents/GitHub/STAT4011-Project-2/data/accident.csv")
-samplesize=60
-x3=data3$x
+data3<-read.csv("data/accident_rawdata.csv")
+x3<-data3[1:samplesize,]
+c1<-as.numeric(gsub(",", "", x3$Total_Accident))
+data3<-matrix(c1,ncol=1)
+colnames(data3)="x"
+x3=as.numeric(data3)
+
 mean_x3=mean(x3)
 len_table=length(2:4)+1
 aic_bic_x3=array(NA,dim = c(2,len_table),dimnames = list(c('aic','bic'),c(paste0('m=',2:4),"selected m")))
@@ -226,7 +238,7 @@ aic_bic_x3[1,len_table]=as.numeric(which.min(aic_bic_x3[1,])+1)
 aic_bic_x3[2,len_table]=as.numeric(which.min(aic_bic_x3[2,])+1)
 print(aic_bic_x3)
 #viterbi
-m=3
+m=4
 initP_last=rep(1/m,m)
 #transP_last=matrix(c(0.1,0.9,0.8,0.2),2,2,byrow = TRUE)
 transP_last=matrix(0.3/m,m,m)+matrix(c(rep(0.7,m),rep(0,m*(m-1))),m,m)
@@ -255,64 +267,24 @@ plot(path,type = "l",col="red",ylab = "",yaxt='n',xlab = "")
 legend("topright", inset=c(-0.4, 0),legend=c("data", "viterbi result"),
        col=c("black", "red"),lty=1:2,cex = 0.6)
 
-#Step 3: Parameter Estimation---------------------------------------------------
-#viterbi x2
-m=as.numeric(aic_bic_x2[1,len_table])
-
-#m=as.numeric(aic_bic_x2[1,len_table])
-m=4
-
-initP_last=rep(1/m,m)
-transP_last=matrix(0.3/m,m,m)+matrix(c(rep(0.7,m),rep(0,m*(m-1))),m,m)
-gaus_mean_last=rep(10,m)
-gaus_sd_last=rep(10,m)
-
-list_mstep_para_gaus_est=EM_gaus(T=length(x2),m,x2,gaus_mean_last,gaus_sd_last,
-                             transP_last,initP_last,
-                             num_ite=10^4,tol=10^(-200))
-
-gaus_mean_est_x2=list_mstep_para_gaus_est$gaus_mean
-gaus_var_est_x2=list_mstep_para_gaus_est$gaus_var
-transP_est_x2=list_mstep_para_gaus_est$transP
-initP_est_x2=list_mstep_para_gaus_est$initP
-
-# viterbi & plot
-path=viterbi_gaus(initP_est_x2,transP_est_x2,gaus_mean_est_x2,gaus_var_est_x2,m,T=length(x2),x2)
-path
-gaus_mean=gaus_mean_est_x2[order(gaus_mean_est_x2)]
-path[path==1]=gaus_mean[1]
-path[path==2]=gaus_mean[2]
-#path[path==3]=gaus_mean[3]
-#path[path==4]=gaus_mean[4]
-path
-par(mar = c(3, 3, 3, 8), xpd = TRUE)
-plot(x2,type = "l",xlab = "time",ylab = "")
-par(new=TRUE)
-plot(path,type = "l",col="red",ylab = "",yaxt='n',xlab = "")
-legend("topright", inset=c(-0.4, 0),legend=c("data", "viterbi result"),
-       col=c("black", "red"),lty=1:2,cex = 0.6)
-
+# density compare
 T=500
 s=rep(NA,T)
 xr=rep(NA,T)
-p_t=initP
+p_t=initP_est_x3
 s[1]=which.max(p_t)
-xr[1]=rnorm(1,gaus_mean[s[1]],sqrt(gaus_var[s[1]]))
+xr[1]=rpois(1,lambda_est_x3[s[1]])
 for (t in 2:T){
-  p_t=initP%*%transP
+  p_t=initP_est_x3%*%transP_est_x3
   s[t]=which.max(p_t)
-  xr[t]=rnorm(1,gaus_mean[s[t]],sqrt(gaus_var[s[t]]))
+  xr[t]=rpois(1,lambda_est_x3[s[t]])
 }
-plot(density(x2))
+par(mar = c(2, 2, 2, 7), xpd = TRUE)
+plot(density(x3))
 par(new=TRUE)
-plot(density(xr),col="red")
-legend("topright", inset=c(-0.4, 0),legend=c("original data", "estimated"),
+plot(density(xr),col="red",yaxt="n",xaxt="n")
+legend("topright", inset=c(-0.25, 0),legend=c("original data", "estimated"),
        col=c("black", "red"),lty=1:2,cex = 0.6)
-#Step 4: Forecast---------------------------------------------------------------
-
-
-#Step 4: Plot-------------------------------------------------------------------
-
 
 
 
