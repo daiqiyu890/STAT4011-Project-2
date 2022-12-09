1d9db8913ecc8d2f90aa152fc5654e1fa29bd98d
Dai Qiyu <83342930+daiqiyu890@users.noreply.github.com>
2022-12-09 17:13:44 +0800

    modify the application code

--- code/Application.R
+++ code/Application.R
@@ -1,57 +1,25 @@
-setwd("/Users/jiangyunhui/Documents/GitHub/STAT4011-Project-2")
-#Step 1: data pre-processing----------------------------------------------------
+rm(list=ls())
+#Step 1: load packages and functions--------------------------------------------
+setwd("C:/Users/Lenovo/Desktop/git/STAT4011-Project-2")
+PackageList =c('abind')
+NewPackages=PackageList[!(PackageList %in%
+                            installed.packages()[,"Package"])]
+if(length(NewPackages)) install.packages(NewPackages)
+lapply(PackageList,require,character.only=TRUE)
 
-"""
-#accident
-data3<-read.csv("data/accident_rawdata.csv")
-x3<-data3[1:samplesize,]
-c1<-as.numeric(gsub(",", "", x3$Total_Accident))
-data3<-matrix(c1,ncol=1)
-colnames(data3)="x"
-"""
-
-#Step 2: Model Selection--------------------------------------------------------
-setwd("/Users/jiangyunhui/Documents/GitHub/STAT4011-Project-2")
 source("code/DGP.r")
 source('code/EM_gaus.r')
 source('code/EM_pois.R')
 source("code/viterbi_gaus.R")
 source("code/viterbi_pois.R")
 
-#sales
-"""
-x2=c(data2$ProductP4)
-gaus_mean_x2=mean(x2)
-gaus_sd_last_x2=sqrt(var(x2))
-len_table=length(2:4)+1
-aic_bic_x2=array(NA,dim = c(2,len_table),dimnames = list(c('aic','bic'),c(paste0('m=',2:4),"selected m")))
+#Step 2: Explore the Energy consumption data------------------------------------
+samplesize=60
+data4=read.csv("data/energy_rawdata.csv")
+data4=data4[1:samplesize,]
+x4=data4[,2]
 
-for (m in 2:4){
-  #m=3
-  initP_last=rep(1/m,m) 
-  transP_last=matrix(0.3/m,m,m)+matrix(c(rep(0.7,m),rep(0,m*(m-1))),m,m)
-  gaus_mean_last=rep(gaus_mean_x2,m)
-  gaus_sd_last=rep(gaus_sd_last_x2,m)
-  
-  list_mstep_para_gaus=EM_gaus(T=length(x2),m,x2,gaus_mean_last,gaus_sd_last,
-                               transP_last,initP_last,
-                               num_ite=10^4,tol=10^(-3))
-
-  aic_x2_m=list_mstep_para_gaus$aic
-  bic_x2_m=list_mstep_para_gaus$bic
-  aic_bic_x2[1,m-1]=aic_x2_m
-  aic_bic_x2[2,m-1]=bic_x2_m
-}
-
-aic_bic_x2[1,len_table]=as.numeric(which.min(aic_bic_x2[1,])+1)
-aic_bic_x2[2,len_table]=as.numeric(which.min(aic_bic_x2[2,])+1)
-print(aic_bic_x2)
-"""
-
-#energy=======================
-data4=read.csv("/Users/jiangyunhui/Documents/GitHub/STAT4011-Project-2/data/energy_first_60.csv")
-x4=data4$x
-plot(density(x4))
+#Step 2.1 model selection
 gaus_mean_x4=mean(x4)
 gaus_sd_last_x4=sqrt(var(x4))
 len_table=length(2:4)+1
@@ -77,7 +45,8 @@ for (m in 2:4){
 aic_bic_x4[1,len_table]=as.numeric(which.min(aic_bic_x4[1,])+1)
 aic_bic_x4[2,len_table]=as.numeric(which.min(aic_bic_x4[2,])+1)
 print(aic_bic_x4)
-# viterbi energy
+
+#Step 2.2 parameter estimation and State Classification
 m=as.numeric(aic_bic_x4[1,len_table])
 initP_last=rep(1/m,m)
 transP_last=matrix(0.3/m,m,m)+matrix(c(rep(0.7,m),rep(0,m*(m-1))),m,m)
@@ -101,14 +70,18 @@ path[path==1]=gaus_mean_est_x4[1]
 path[path==2]=gaus_mean_est_x4[2]
 path[path==3]=gaus_mean_est_x4[3]
 #path[path==4]=gaus_mean[4]
-path
+
+#Plot the path
+png("output/viterbi_energy.png",width=800,height=400)
 par(mar = c(3, 3, 3, 8), xpd = TRUE)
 plot(x4,type = "l",xlab = "time",ylab = "")
 par(new=TRUE)
-plot(path,type = "l",col="red",ylab = "",yaxt='n',xlab = "")
-legend("topright", inset=c(-0.4, 0),legend=c("data", "viterbi result"),
-       col=c("black", "red"),lty=1:2,cex = 0.6)
-# density compare
+plot(path,type = "l",col="red",ylab = "state/data",yaxt='n',xlab = "time",
+     main="estimated hidden state")
+dev.off()
+
+
+#Step 2.3 density compare-------------------------------------------------------
 T=500
 s=rep(NA,T)
 xr=rep(NA,T)
@@ -120,93 +93,21 @@ for (t in 2:T){
   s[t]=which.max(p_t)
   xr[t]=rnorm(1,gaus_mean_est_x4[s[t]],sqrt(gaus_var_est_x4[s[t]]))
 }
+
+png("output/energy_density.png",width=800,height=400)
 par(mar = c(2, 2, 2, 7), xpd = TRUE)
 plot(density(x4))
 par(new=TRUE)
-plot(density(xr),col="red",yaxt="n",xaxt="n")
-legend("topright", inset=c(-0.25, 0),legend=c("original data", "estimated"),
+plot(density(xr),col="red",yaxt="n",xaxt="n",
+     main="comparison between simulation and real data")
+legend("topright", inset=c(-0.25, 0),legend=c("real data","simulated data"),
        col=c("black", "red"),lty=1:2,cex = 0.6)
-#pois==============================
-# visitor
-#model select
-data1<-read.csv("/Users/jiangyunhui/Documents/GitHub/STAT4011-Project-2/data/visitors_rawdata.csv")
+dev.off()
+
+
+#Step 3: Explore the accident data----------------------------------------------
+#Step 3.1 model select
 samplesize=60
-a<-nrow(data1)
-b<-a-(samplesize-1)
-x1<-data1[b:a,]
-x1<-as.numeric(gsub(",", "", x1$Unique.Visits))
-mean_x1=mean(x1)
-len_table=length(2:4)+1
-aic_bic_x1=array(NA,dim = c(2,len_table),dimnames = list(c('aic','bic'),c(paste0('m=',2:4),"selected m")))
-
-for (m in 2:4){
-  #m=3
-  initP_last=rep(1/m,m)
-  transP_last=matrix(0.3/m,m,m)+matrix(c(rep(0.7,m),rep(0,m*(m-1))),m,m)
-  lambda_last=rep(mean_x1,m)
-  
-  list_mstep_para_pois=EM_pois(T=length(x1),m,x1,lambda_last,
-                               transP_last,initP_last,
-                               num_ite=10^4,tol=10^(-30))
-  
-  aic_x1_m=list_mstep_para_pois$aic
-  bic_x1_m=list_mstep_para_pois$bic
-  aic_bic_x1[1,m-1]=aic_x1_m
-  aic_bic_x1[2,m-1]=bic_x1_m
-}
-
-aic_bic_x1[1,len_table]=as.numeric(which.min(aic_bic_x1[1,])+1)
-aic_bic_x1[2,len_table]=as.numeric(which.min(aic_bic_x1[2,])+1)
-print(aic_bic_x1)
-#viterbi
-m=as.numeric(aic_bic_x1[1,len_table])
-initP_last=rep(1/m,m)
-transP_last=matrix(0.3/m,m,m)+matrix(c(rep(0.7,m),rep(0,m*(m-1))),m,m)
-lambda_last=rep(3000,m)
-
-list_mstep_para_pois=EM_pois(T=length(x1),m,x1,lambda_last,
-                             transP_last,initP_last,
-                             num_ite=10^4,tol=10^(-30))
-
-lambda_est_x1=list_mstep_para_pois$lambda
-transP_est_x1=list_mstep_para_pois$transP
-initP_est_x1=list_mstep_para_pois$initP
-
-path=viterbi_pois(initP_est_x1,transP_est_x1,lambda_est_x1,m,T=length(x1),x1)
-path
-#lambda_x1=lambda_est_x1[order(gaus_mean_est_x2)]
-path[path==1]=lambda_est_x1[1]
-path[path==2]=lambda_est_x1[2]
-#path[path==3]=gaus_mean[3]
-#path[path==4]=gaus_mean[4]
-path
-par(mar = c(3, 3, 3, 8), xpd = TRUE)
-plot(x1,type = "l",xlab = "time",ylab = "")
-par(new=TRUE)
-plot(path,type = "l",col="red",ylab = "",yaxt='n',xlab = "")
-legend("topright", inset=c(-0.4, 0),legend=c("data", "viterbi result"),
-       col=c("black", "red"),lty=1:2,cex = 0.6)
-
-# density compare
-T=500
-s=rep(NA,T)
-xr=rep(NA,T)
-p_t=initP_est_x1
-s[1]=which.max(p_t)
-xr[1]=rpois(1,lambda_est_x1[s[1]])
-for (t in 2:T){
-  p_t=initP_est_x1%*%transP_est_x1
-  s[t]=which.max(p_t)
-  xr[t]=rpois(1,lambda_est_x1[s[t]])
-}
-par(mar = c(2, 2, 2, 7), xpd = TRUE)
-plot(density(x1))
-par(new=TRUE)
-plot(density(xr),col="red",yaxt="n",xaxt="n")
-legend("topright", inset=c(-0.25, 0),legend=c("original data", "estimated"),
-       col=c("black", "red"),lty=1:2,cex = 0.6)
-#accident===============================
-#model select
 data3<-read.csv("data/accident_rawdata.csv")
 x3<-data3[1:samplesize,]
 c1<-as.numeric(gsub(",", "", x3$Total_Accident))
@@ -222,7 +123,7 @@ for (m in 2:4){
   #m=2
   initP_last=rep(1/m,m)
   transP_last=matrix(0.3/m,m,m)+matrix(c(rep(0.7,m),rep(0,m*(m-1))),m,m)
-  lambda_x3=rep(mean_x3,m)
+  lambda_last=rep(mean_x3,m)
   
   list_mstep_para_pois=EM_pois(T=length(x3),m,x3,lambda_last,
                                transP_last,initP_last,
@@ -230,14 +131,15 @@ for (m in 2:4){
   
   aic_x3_m=list_mstep_para_pois$aic
   bic_x3_m=list_mstep_para_pois$bic
-  aic_bic_x3[1,m-1]=aic_x1_m
-  aic_bic_x3[2,m-1]=bic_x1_m
+  aic_bic_x3[1,m-1]=aic_x3_m
+  aic_bic_x3[2,m-1]=bic_x3_m
 }
 
 aic_bic_x3[1,len_table]=as.numeric(which.min(aic_bic_x3[1,])+1)
 aic_bic_x3[2,len_table]=as.numeric(which.min(aic_bic_x3[2,])+1)
 print(aic_bic_x3)
-#viterbi
+
+#Step 3.2 parameter estimation and viterbi algorithm
 m=4
 initP_last=rep(1/m,m)
 #transP_last=matrix(c(0.1,0.9,0.8,0.2),2,2,byrow = TRUE)
@@ -260,14 +162,17 @@ path[path==2]=lambda_est_x3[2]
 path[path==3]=lambda_est_x3[3]
 #path[path==4]=gaus_mean[4]
 path
+
+png("output/viterbi_accident.png",width=800,height=400)
 par(mar = c(3, 3, 3, 8), xpd = TRUE)
 plot(x3,type = "l",xlab = "time",ylab = "")
 par(new=TRUE)
 plot(path,type = "l",col="red",ylab = "",yaxt='n',xlab = "")
 legend("topright", inset=c(-0.4, 0),legend=c("data", "viterbi result"),
        col=c("black", "red"),lty=1:2,cex = 0.6)
+dev.off()
 
-# density compare
+#Step 3.3 density compare
 T=500
 s=rep(NA,T)
 xr=rep(NA,T)
@@ -279,13 +184,12 @@ for (t in 2:T){
   s[t]=which.max(p_t)
   xr[t]=rpois(1,lambda_est_x3[s[t]])
 }
+
+png("output/accident_density.png",width=800,height=400)
 par(mar = c(2, 2, 2, 7), xpd = TRUE)
 plot(density(x3))
 par(new=TRUE)
 plot(density(xr),col="red",yaxt="n",xaxt="n")
 legend("topright", inset=c(-0.25, 0),legend=c("original data", "estimated"),
        col=c("black", "red"),lty=1:2,cex = 0.6)
-
-
-
-
+dev.off()
